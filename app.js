858c0169b2d9031c7ed5078a3f69baa88016b016
Nicolas Figueroa <nicolasfigb@gmail.com>
2022-12-07 20:27:10 -0500

    Added form interactivity

---
+++ app.js
@@ -0,0 +1,16 @@
+let pre_submitted = document.querySelector(".pre-submitted");
+let submitted = document.querySelector(".submitted-form");
+let submit_btn = document.querySelector(".submit");
+let final_score = document.querySelector(".num");
+let scores = document.querySelectorAll(".score-btn");
+
+submit_btn.addEventListener("click", () => {
+  pre_submitted.style.display = "none";
+  submitted.style.display = "flex";
+});
+
+scores.forEach((score) => {
+  score.addEventListener("click", () => {
+    final_score.innerHTML = score.innerHTML;
+  });
+});
