3af12e0aa02e57f38b5814d5dd4933549e5fff69
Nicolas Figueroa <nicolasfigb@gmail.com>
2022-12-08 19:24:39 -0500

    Submit only if score is selected

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ let submit_btn = document.querySelector(".submit");
 let final_score = document.querySelector(".num");
 let scores = document.querySelectorAll(".score-btn");
 
-submit_btn.addEventListener("click", () => {
-  pre_submitted.style.display = "none";
-  submitted.style.display = "flex";
-});
-
 scores.forEach((score) => {
   score.addEventListener("click", () => {
     final_score.innerHTML = score.innerHTML;
